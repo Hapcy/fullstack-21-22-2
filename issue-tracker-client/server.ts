3f681d5e11c01cd6ab910104a450f6295947bc89
tibor.moger <tibor.moger@smartx.eu>
2021-12-07 22:55:14 +0100

    kliens deploy basic

---
+++ issue-tracker-client/server.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const app = express();
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'localhost:3000',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '',
+    },
+  })
+);
+
+app.use(express.static('./dist/issue-tracker-client'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve('./dist/issue-tracker-client/index.html'));
+});
+
+app.listen(process.env.PORT || 4200);
